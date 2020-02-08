07f905456a523a59f2807836233ddcd901309cd1
jlissner <jlissner@gmail.com>
2020-02-08 13:02:48 -0600

    working on getting nodemon working

--- routes.js
+++ routes.js
@@ -5,12 +5,16 @@ const expressWs = require('express-ws');
 module.exports = app => {
   const wss = expressWs(app);
 
-  router.ws('/echo', (ws, req) => {
-    ws.on('message', (msg) => wss.getWss().clients.forEach(client => {
+  wss.broadcast = (msg) => {
+    wss.getWss().clients.forEach(client => {
       if (client.readyState === 1) {
         client.send(msg)
       }
-    }));
+    });
+  };
+
+  router.ws('/echo', (ws, req) => {
+    ws.on('message', wss.broadcast);
   });
 
   router.get('/ping', function (req, res) {
