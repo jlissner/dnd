274182b6143283364589da8015741f9274f5a65e
jlissner <jlissner@gmail.com>
2020-03-27 12:23:32 -0500

    created build specific webpack, will need to dig into reacts webpack later to make it work for me

---
+++ config/webpack.build.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    path.resolve('./client/index'),
+  ],
+  mode: 'production',
+  resolve: {
+    extensions: [
+      '.js',
+      '.jsx',
+      '.json',
+    ],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+        include: [/client/],
+      }, {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve('./server/build'),
+    filename: 'static/js/[name].[contenthash:8].js',
+    // publicPath: '/',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(
+      Object.assign(
+        {},
+        {
+          inject: true,
+          template: path.resolve('./server/public/index.html'),
+        },
+        {
+          minify: {
+            removeComments: true,
+            collapseWhitespace: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true,
+            removeEmptyAttributes: true,
+            removeStyleLinkTypeAttributes: true,
+            keepClosingSlash: true,
+            minifyJS: true,
+            minifyCSS: true,
+            minifyURLs: true,
+          },
+        }
+      )
+    ),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
+      },
+    }),
+    new webpack.IgnorePlugin(/regenerator|nodent|js-beautify/, /ajv/),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+  ],
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
--- server/index.js
+++ server/index.js
@@ -37,7 +37,6 @@ app.use(require('./routes'))
 app.use(express.static(path.resolve('./server/build')));
 
 if (process.env.NODE_ENV === 'development') {
-  console.log('here??')
   const webpack = require('webpack');
   const webpackConfigFactory = require('../config/webpack.config');
   const webpackDevMiddleware = require('webpack-dev-middleware');
